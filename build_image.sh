8a723a98155216f81e52cf736d4e543414559c5a
Joachim Veulemans <joachim@cloudclass.be>
2019-09-28 12:19:08 +0100

    Changed Dockerfiles to have an NVIDIA or OpenGL base image

--- build_image.sh
+++ build_image.sh
@@ -5,7 +5,9 @@
 vendor=`glxinfo | grep vendor | grep OpenGL | awk '{ print $4 }'`
 
 if [ $vendor == "NVIDIA" ]; then
-    (docker build -t px4_sitl_docker_nvidia:latest -f ./dockerfile/nvidia/Dockerfile .)
+    (docker build -t px4_sitl_docker_base:latest -f ./dockerfile/nvidia/Dockerfile .)
 else
-    (docker build -t px4_sitl_docker_opengl:latest -f ./dockerfile/opengl/Dockerfile .)
+    (docker build -t px4_sitl_docker_base:latest -f ./dockerfile/opengl/Dockerfile .)
 fi
+
+(docker build -t px4_sitl_docker:latest -f ./dockerfile/px4/Dockerfile .)
